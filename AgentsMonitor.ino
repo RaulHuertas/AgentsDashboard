33406cc585159616775b13ebc16d337eac4c9d1b
Raul Huertas <rax20037@gmail.com>
2026-08-23 22:32:25 -0500

    AT commands working

--- AgentsMonitor.ino
+++ AgentsMonitor.ino
@@ -3,38 +3,33 @@
 #define USE_TFT_ESPI_LIBRARY
 #include "lv_xiao_round_screen.h"
 #include "AgentsDashboard.h"
+#include "ATCommandScanner.h"
 
 static bool initialStatusesApplied = false;
 static unsigned long startupMs = 0;
 
-static void applyInitialAgentStatuses() {
-  setStatusCircleAnimation(2, true);
-  setStatusCircleLabel(0, "IntelliJ");
-  setStatusCircleLabel(1, "Nvim");
-  setStatusCircleLabel(2, "OpenCode");
-  setStatusCircleLabel(3, "VSCode");
-}
-
 void setup() {
   // put your setup code here, to run once:
   Serial.begin( 115200 );  //prepare for possible serial debug 
-  Serial.println( "XIAO round screen - LVGL_Arduino" );
+  //Serial.println( "XIAO round screen - LVGL_Arduino" );
 
   lv_init();
   lv_xiao_disp_init();
   lv_xiao_touch_init();
   initializeDashboard();
+  initATCommandScanner(Serial);
   startupMs = millis();
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  pollATCommandScanner();
   lv_timer_handler();  //let the GUI do its work 
   unsigned long now = millis();
-  if (!initialStatusesApplied && ( - startupMs >= 2000)) {
-    applyInitialAgentStatuses();
+  if (!initialStatusesApplied && (now - startupMs >= 2000)) {
+    
     initialStatusesApplied = true;
-    Serial.println("Status update");
+    //Serial.println("Status update");
   }
 
   delay( 5 );
