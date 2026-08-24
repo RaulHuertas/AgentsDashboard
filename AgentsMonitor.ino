5bd09b4b97a024f68188e138700df5707d3a0427
Raul Huertas <rax20037@gmail.com>
2026-08-23 22:08:31 -0500

    Simplifying text update code

--- AgentsMonitor.ino
+++ AgentsMonitor.ino
@@ -4,6 +4,17 @@
 #include "lv_xiao_round_screen.h"
 #include "AgentsDashboard.h"
 
+static bool initialStatusesApplied = false;
+static unsigned long startupMs = 0;
+
+static void applyInitialAgentStatuses() {
+  setStatusCircleAnimation(2, true);
+  setStatusCircleLabel(0, "IntelliJ");
+  setStatusCircleLabel(1, "Nvim");
+  setStatusCircleLabel(2, "OpenCode");
+  setStatusCircleLabel(3, "VSCode");
+}
+
 void setup() {
   // put your setup code here, to run once:
   Serial.begin( 115200 );  //prepare for possible serial debug 
@@ -13,17 +24,19 @@ void setup() {
   lv_xiao_disp_init();
   lv_xiao_touch_init();
   initializeDashboard();
-  setStatusCircleAnimation(3, true);
-  setStatusCircleLabel(0, "IntelliJ");
-  setStatusCircleLabel(1, "Nvim");
-  setStatusCircleLabel(2, "OpenCode");
-  setStatusCircleLabel(3, "VSCode");
+  startupMs = millis();
 }
 
-static bool a = false;
 void loop() {
   // put your main code here, to run repeatedly:
   lv_timer_handler();  //let the GUI do its work 
+  unsigned long now = millis();
+  if (!initialStatusesApplied && ( - startupMs >= 2000)) {
+    applyInitialAgentStatuses();
+    initialStatusesApplied = true;
+    Serial.println("Status update");
+  }
+
   delay( 5 );
 
 }
